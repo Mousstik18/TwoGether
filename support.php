2d193eb41dafec6002bee0646bf0deaf00026124
Mousstik <Mousstik18@gmail.com>
2020-03-27 14:29:24 +0100

    Add discord widget on support pagesand custom css

--- support.php
+++ support.php
@@ -2,5 +2,16 @@
 <html>
 <?php include('head.php') ?>
 
-<body  style="font-family: 'Mansalva', cursive;>
+<body  style="font-family: 'Mansalva', cursive;">
     <?php include('navbar.php') ?>
+    
+    <div class="form-support">
+        <widgetbot
+          server="147994383042609152"
+          channel="693087603846807603"
+          width="1280"
+          height="720"
+          shard="https://disweb.dashflo.net">
+        </widgetbot>
+<script src="https://cdn.jsdelivr.net/npm/@widgetbot/html-embed"></script>
+    </div>
