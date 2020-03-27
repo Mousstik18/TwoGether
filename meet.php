2818275055f270368383de23bdba985a07302fe0
Mousstik <Mousstik18@gmail.com>
2020-03-27 13:45:50 +0100

    preparation des pages

--- meet.php
+++ meet.php
@@ -4,6 +4,3 @@
 
 <body  style="font-family: 'Mansalva', cursive;">
     <?php include('navbar.php') ?>
-
-    
-    <img style="background-image: url("web.png"); background-repeat: no-repeat; background-position: center; background-size: auto"
--- support.php
+++ support.php
@@ -2,8 +2,5 @@
 <html>
 <?php include('head.php') ?>
 
-<body  style="font-family: 'Mansalva', cursive;">
+<body  style="font-family: 'Mansalva', cursive;>
     <?php include('navbar.php') ?>
-
-    
-    <img style="background-image: url("web.png"); background-repeat: no-repeat; background-position: center; background-size: cover"
